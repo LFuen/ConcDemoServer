e34d406d70bf5abe25628d2e70ecc34c8539f753
Lili Fuentes <lile7886@gmail.com>
2021-05-30 18:04:01 -0400

    changed all settings

--- src/config.js
+++ src/config.js
@@ -1,11 +1,9 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    JWT_SECRET: 'secretkey',
+    JWT_SECRET: 's3cr3tKey',
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://phalanx-client.vercel.app', // https://concretti-application.vercel.app/
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://democlient.vercel.app', // http://localhost:3000
     DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgresql://dundermiffling:1Pepperm%40n@localhost/phalanx",
-  TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL || ''
+    "postgres://ailpibpwyoagvx:ce282fd299e9832b41dc2717eec03331d9605866f4403d672cd6894e119516f2@ec2-54-160-96-70.compute-1.amazonaws.com:5432/d46dlro4856gj1",
 }
